4686b1f6372b6a101232b213d0eb622c214b07c0
kay-134 <kaykayc0103@gmail.com>
2020-06-20 12:07:34 -0400

    please work

--- krazycoaches/app/src/main/java/com/example/krazycoaches/MainActivity.java
+++ krazycoaches/app/src/main/java/com/example/krazycoaches/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 //this is a testing akeena comment 
     //hello there Akeena ; AHHHH SNAPPPPPP, HEY SIIIS 
